786f1f857fd6b2f12c1f15e6503c19dd9e9cce16
Rami Issa <ram_fad@yahoo.com>
2025-02-22 00:01:39 +0200

    Implemented basic threading example in MultiThreadingDemo using top-level statements.

--- MultiThreadingDemo/Program.cs
+++ MultiThreadingDemo/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+Console.WriteLine("Main Thread Started");
+Thread thread = new Thread(PrintNumbers);
+thread.Start();
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Main Thread: {i}");
+    Thread.Sleep(500);
+}
+
+Console.WriteLine("Main Thread Ended");
+//Console.ReadLine();
+void PrintNumbers()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        Console.WriteLine($"Worker Thread: {i}");
+        Thread.Sleep(1000); // Simulate some work
+    }
+}
